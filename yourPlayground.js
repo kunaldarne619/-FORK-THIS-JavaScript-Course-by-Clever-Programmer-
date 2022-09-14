49a071d70941f63dffe362af1dafcc10f2418a4f
kunal darne <76103735+kunaldarne619@users.noreply.github.com>
2022-09-14 13:13:52 +0000

    your

--- yourPlayground.js
+++ yourPlayground.js
@@ -1,12 +1,12 @@
-// variable
-name="kunal";
-console.log(name)
+// variable var ,let ,const
+//name="kunal";
+//console.log(name)
 
 //operators
 //fruit =  prompt('fruit name')
 //console.log(fruit)
 //* /** + -
-food=Number(prompt('How much was the food ?'))
+/*food=Number(prompt('How much was the food ?'))
 tip=Number(prompt('Tip percentage'))/100
 tipAmount= food *tip
 total= food +tipAmount
@@ -14,6 +14,38 @@ total= food +tipAmount
 console.log(`Tip Amount is : ${tipAmount}`)
 console.log(`total Amount is : ${total}`)
 //alert(tipAmount)
+// Data Type(Nummber and strings)/*
+
+/*
+number 1,2,2,3,1,3;
+String 'kunal' "darne" 
+Array = []
+Object ={}
+Bollen = true/False*/
+
+
+/*Maths Stuff
+ Maths Operators
+ multiply *
+ divide /
+ Exponent **
+ modulle % 5%2  */
+
+/*Maths Method
+Math.floor= Round Down
+Math.ceil()=Round Up
+Math.Random()=Random Number*/
+
+//baby wwather app (Conditional)
+//if rain "grab your umbrella"
+//else "Wear Your SunGlasses"
+let weather=prompt(`weather is`)
+if(weather =='rain'){
+  console.log("grab your umbrella");
+}else{
+  console.log("Wear Your SunGlasses")
+}
+
 
 //function
 
