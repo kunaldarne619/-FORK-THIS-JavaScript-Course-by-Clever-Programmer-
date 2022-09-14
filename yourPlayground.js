0828e189131977ef16fab7d8099241426308c8c0
kunal darne <76103735+kunaldarne619@users.noreply.github.com>
2022-09-14 12:43:26 +0000

    Your playground is changes

---
+++ yourPlayground.js
@@ -0,0 +1,20 @@
+// variable
+name="kunal";
+console.log(name)
+
+//operators
+//fruit =  prompt('fruit name')
+//console.log(fruit)
+//* /** + -
+food=Number(prompt('How much was the food ?'))
+tip=Number(prompt('Tip percentage'))/100
+tipAmount= food *tip
+total= food +tipAmount
+
+console.log(`Tip Amount is : ${tipAmount}`)
+console.log(`total Amount is : ${total}`)
+//alert(tipAmount)
+
+//function
+
+
